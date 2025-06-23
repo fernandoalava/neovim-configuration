7267a14d9faf2e1da83f7c9492b43a1864c5cb9e
Fernando Alava <falava@gmail.com>
2025-06-23 19:12:23 +0300

    feat: added rose-pine theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,7 +1,13 @@
 return {
-	"rebelot/kanagawa.nvim",
+	--[["rebelot/kanagawa.nvim",
 	name = "kanagawa",
 	config = function()
 		require("kanagawa").load("wave")
+	end,
+  ]]
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		vim.cmd("colorscheme rose-pine")
 	end,
 }
