7f8f8b810adcd7612691b7fe0abee94e604b0afa
Fernando Alava <falava@gmail.com>
2025-06-23 19:18:29 +0300

    add: reverted theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,13 +1,7 @@
 return {
-	--[["rebelot/kanagawa.nvim",
+	"rebelot/kanagawa.nvim",
 	name = "kanagawa",
 	config = function()
 		require("kanagawa").load("wave")
-	end,
-  ]]
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		vim.cmd("colorscheme rose-pine")
 	end,
 }
