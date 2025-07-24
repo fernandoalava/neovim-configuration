fd11e989fb218122176ad47748bbd71280cf610e
Fernando Alava <falava@gmail.com>
2025-07-24 17:50:20 +0300

    feat: added new theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,12 +1,6 @@
 return {
-  "nyoom-engineering/oxocarbon.nvim",
-  name = "oxocarbon",
-  config = function()
-    -- NO OP
-  end,
-  init = function()
-    -- vim.opt.termguicolors = true
-    -- vim.opt.background = 'dark'
-    vim.cmd.colorscheme 'oxocarbon'
-  end
+	"scottmckendry/cyberdream.nvim",
+	config = function()
+		vim.cmd.colorscheme("cyberdream")
+	end,
 }
